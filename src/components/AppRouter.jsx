86d6d380ddb5a61b19279ac3d3b9cb315c098404
Maxim Lukashenko <max.lukashenko2017@gmail.com>
2022-12-07 19:59:46 +0300

    add auth context

--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -1,14 +1,27 @@
+import { useContext } from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import { authRoutes, publicRoutes } from "../routes";
+import { HOME_ROUTE, LOGIN_ROUTE } from "../utils/consts";
+
+import { AuthContext } from "../index";
 
 import Home from "../pages/Home";
 
 const AppRouter = () => {
+  const isAuth = useContext(AuthContext)
+
+  console.log(isAuth)
+  
   return (
     <Routes>
-      {authRoutes.map(({path, element}) => <Route key={path} path={path} element={element} />)}
+      {!isAuth && authRoutes.map(({path, element}) => <Route key={path} path={path} element={element} />)}
 
-      {publicRoutes.map(({path, element}) => <Route key={path} path={path} element={element} /> )}
+      {isAuth && publicRoutes.map(({path, element}) => <Route key={path} path={path} element={element} /> )}
+      
+      <Route 
+        element={<Navigate to={isAuth ? HOME_ROUTE : LOGIN_ROUTE} />}
+        path="*"
+      />
     </Routes>
   )
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { createContext } from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+export const AuthContext = createContext(null);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <AuthContext.Provider value={true}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </AuthContext.Provider>
 );

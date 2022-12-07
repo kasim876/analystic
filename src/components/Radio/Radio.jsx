d5c8450661d4fad1b14a1d2dcb428657bf4e56c3
Maxim Lukashenko <max.lukashenko2017@gmail.com>
2022-12-07 19:33:26 +0300

    edit radio component

--- src/components/Radio/Radio.jsx
+++ src/components/Radio/Radio.jsx
@@ -2,7 +2,7 @@ import styles from './Radio.module.scss';
 
 const Radio = (props) => {
   return (
-    <label className={styles.radio}>
+    <label className={styles.root}>
       <input type="radio" name={props.name} value={props.value} className={styles.input} onChange={props.change} />
       <span className={'text-md' + ' ' + styles.caption}>{props.caption}</span>
     </label>
